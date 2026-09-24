c19a2f6d7ef8ba2f4ea968863a3ce90d401efb3a
Trinath Kella <kellatrinath@gmail.com>
2026-09-24 14:27:15 +0530

    SPI master RTL code with the initial code development of tb

---
+++ spi_master.sv
@@ -0,0 +1,175 @@
+typedef enum logic [1:0] {
+	IDLE 		= 2'b00,
+	WAIT_START	= 2'b01,
+	SHIFT 		= 2'b10,
+	WAIT_END 	= 2'b11
+}state_e;
+
+module spi_master #(parameter A_WIDTH = 6, 
+					parameter RD_D_WIDTH = 24, 
+					parameter WR_D_WIDTH = 8, 
+					parameter D_WIDTH = 32, 
+					parameter CLK_DIV = 2, 
+					parameter CPOL = 0,
+					parameter SETUP_CYCLES = 3,
+					parameter HOLD_CYCLES  = 3)
+(
+	// Control Signals
+	input	wire	clk,					// 80 MHz
+	input	wire	rst_n,
+	// Design Time Inf Signals
+	input	wire	start,
+	input 	wire	[A_WIDTH - 1 : 0] addr,
+	input	wire	rw,
+	input 	wire 	auto_inc,
+	input 	wire 	width_flag,						// 0 = 16, 1 = 32
+	input 	wire 	[WR_D_WIDTH - 1 : 0] wr_data,
+	output 	reg 	[RD_D_WIDTH - 1 : 0] rd_data,
+	output  reg		done,
+	output  wire    busy,	// Indiactes the TXN is under processed or can go for the nxt TXN
+	// Physical Inf
+	output	reg		sclk,
+	output  reg		csb,
+	output 	reg 	din,
+	input 	wire 	dout
+);
+	// ENUM
+	state_e state_ps, state_ns;
+	// Internal registers
+	reg  [D_WIDTH - 1 : 0] shift_reg;
+	reg  [A_WIDTH - 1 : 0] edge_count, wait_count;	// Edge_Count for counting the sclk edges, wait_count - counter reg for WAIT_START || WAIT_END
+	reg [$clog2(CLK_DIV) - 1 : 0] clk_div_count;	// For dividing the sys_clk to the sclk
+	// WIres
+	wire sclk_tick;									// for definig the sampling/driving with the help of sclk currently value
+	wire [A_WIDTH - 1 : 0] target_wait;				// To assign the SETUP or HOLD wait cycles
+	wire [$clog2(D_WIDTH) : 0] target_edge;			// To assign the 16 or 32 edges count
+	/*-----------------------------------------------------------------------------*/
+	
+	assign target_wait = (state_ps == WAIT_START) ? SETUP_CYCLES : HOLD_CYCLES;
+	assign target_edge = (width_flag) ? D_WIDTH : D_WIDTH >> 1;
+ 
+	// CLock Division Logic ////////////////////////////////////////////////////////
+	always_ff @ (posedge clk, negedge rst_n)
+	begin
+		if(!rst_n) begin
+			clk_div_count <= 'd0;
+			sclk 		  <= CPOL;
+		end
+		else begin 
+			if(state_ps == SHIFT) begin
+				if(clk_div_count == CLK_DIV - 1) begin
+					clk_div_count <= 'd0;
+					sclk 		  <= ~sclk;
+				end
+				else
+					clk_div_count <= clk_div_count + 1;
+			end
+			else begin
+				clk_div_count	  <= 'd0;
+				sclk  			  <= CPOL;
+			end
+		end
+	end
+
+	assign sclk_tick = (state_ps == SHIFT) && (clk_div_count == CLK_DIV - 1);
+	////////////////////////////////////////////////////////////////////////////////
+
+	// WAIT_COUNTER ///////////////////////////////////////////////////////////////
+	always_ff @ (posedge clk or negedge rst_n)
+	begin
+		if(!rst_n)
+			wait_count <= 'd0;
+		else if(state_ps == WAIT_START || state_ps == WAIT_END) begin
+			if(wait_count == target_wait - 1)
+				wait_count <= 'd0;
+			else
+				wait_count <= wait_count + 1;
+		end
+		else
+			wait_count <= 'd0;
+	end
+	////////////////////////////////////////////////////////////////////////////////
+
+	// SHIFTING ///////////////////////////////////////////////////////////////////
+		always_ff @ (posedge clk , negedge rst_n)
+		begin
+			if(!rst_n) begin
+				edge_count	<=	'd0;
+				shift_reg 	<=	'd0;
+				din 		<=	1'b0;
+			end
+			else if(state_ps == IDLE && start) begin
+				shift_reg	<=	{auto_inc, rw, addr, (rw ? wr_data : 8'd0), 16'd0};
+				din 		<=	auto_inc;
+			end
+			else if (state_ps == SHIFT) begin
+				if(sclk_tick && sclk) 						// FALLING EDGE - Driving the DIN a bit to the SLAVE TDC7200
+					din 	  <= shift_reg[D_WIDTH - 1];
+				else if(sclk_tick && !sclk) begin 			// RISING EDGE - Sampling the bit from SLAVE TDC7200 through DOUT
+					shift_reg  <= {shift_reg[D_WIDTH - 2 : 0], dout};
+					if(edge_count == target_edge - 1)
+						edge_count <= 'd0;
+					else
+						edge_count <= edge_count + 1;
+				end
+			end
+		end
+	////////////////////////////////////////////////////////////////////////////////
+
+	// State Register Logic
+	always_ff @(posedge clk or negedge rst_n)
+	begin
+		if(!rst_n)
+			state_ps	<=	IDLE;
+		else
+			state_ps	<=	state_ns;
+	end
+
+	// State Transfer Logic
+	always_comb begin
+		csb = 1'b1;
+		state_ns = state_ps;
+		case(state_ps)
+			IDLE : state_ns = (start) ? WAIT_START : IDLE;
+			WAIT_START : begin
+				csb = 1'b0;
+				state_ns = (wait_count == target_wait - 1) ? SHIFT : WAIT_START;
+			end
+			SHIFT : begin
+				csb      = 1'b0;
+				state_ns = (edge_count == target_edge - 1) ? WAIT_END : SHIFT;
+			end
+			WAIT_END : begin
+				csb = 1'b0;
+				if(wait_count == target_wait - 1) begin
+					csb = 1'b1;
+					state_ns = IDLE;
+				end
+				else begin
+					state_ns = WAIT_END;
+				end
+			end
+			default : state_ns = IDLE;
+		endcase
+	end
+
+	// Output Logic
+	always_ff @ (posedge clk, negedge rst_n)
+	begin
+		if(!rst_n) begin
+			rd_data 	<=	'd0;
+			done		<=	1'b0;
+		end
+		else begin
+			if(state_ps == WAIT_END && wait_count == target_wait - 1) begin
+				rd_data		<=	shift_reg[RD_D_WIDTH - 1 : 0];
+				done		<=	1'b1;
+			end
+			else
+				done		<=	1'b0;
+		end
+	end
+
+	assign busy = (state_ps != IDLE);
+
+endmodule : spi_master
---
+++ test_bench/tb_spi_master.sv
@@ -0,0 +1,97 @@
+`include "E:/Trinath_Kella/modules/spi_master/spi_master.sv"
+
+module tb_spi_master;
+
+	parameter A_WIDTH = 6;
+	parameter RD_D_WIDTH = 24; 
+	parameter WR_D_WIDTH = 8;
+	parameter D_WIDTH = 32;
+	parameter CLK_DIV = 8;
+	parameter CPOL = 0;
+	parameter SETUP_CYCLES = 3;
+	parameter HOLD_CYCLES  = 3;
+
+	reg clk, rst_n;
+	reg start, rw, auto_inc, width_flag;
+	reg  [A_WIDTH - 1 : 0] addr;
+	reg  [WR_D_WIDTH - 1 : 0] wr_data;
+	wire [RD_D_WIDTH - 1 : 0] rd_data;
+	wire done, busy;
+
+	// Physical Interface Pins
+	wire sclk, csb, din;
+	reg  dout;
+
+	// Expected Result
+	reg [(2 * WR_D_WIDTH) - 1 : 0] expected_val;
+
+	reg [A_WIDTH - 2 : 0] cntr;
+
+	spi_master #(.A_WIDTH(A_WIDTH), .RD_D_WIDTH(RD_D_WIDTH),
+				 .WR_D_WIDTH(WR_D_WIDTH), .D_WIDTH(D_WIDTH),
+				 .CLK_DIV(CLK_DIV), .CPOL(CPOL), .SETUP_CYCLES(SETUP_CYCLES),
+				 .HOLD_CYCLES(HOLD_CYCLES))
+	dut
+	(
+		.clk       (clk),
+		.rst_n     (rst_n),
+		.start     (start),
+		.addr      (addr),
+		.rw        (rw),
+		.auto_inc  (auto_inc),
+		.width_flag(width_flag),
+		.wr_data   (wr_data),
+		.rd_data   (rd_data),
+		.done      (done),
+		.busy      (busy),
+		// Physical Interface Pins
+		.sclk      (sclk),
+		.csb       (csb),
+		.din       (din),
+		.dout      (dout)
+	);
+
+	// Clock - 80 MHz
+	always #6.25 clk = !clk;
+	initial clk = 0;
+
+	always @ (posedge sclk) begin
+		if (!csb) begin
+			if (expected_val[((2 * WR_D_WIDTH) - 1) - cntr] == din)
+				$display("[%0t] PASS: bit %0d - expected=%b actual(din)=%b",
+				          $time, cntr, expected_val[((2*WR_D_WIDTH)-1)-cntr], din);
+			else
+				$display("[%0t] FAIL: bit %0d - expected=%b actual(din)=%b",
+				          $time, cntr, expected_val[((2*WR_D_WIDTH)-1)-cntr], din);
+
+			cntr <= cntr + 1;
+		end
+	end
+
+	// Test Bench
+	initial begin
+		rst_n = 1'b0;
+		cntr  = 1'b0;
+		repeat(2) @(posedge clk);
+		rst_n = 1'b1;
+
+		// Driving the addr, rw, auto_inc, wr_data (before start asserts because we can remove the ambiguity of the start reading the older values of these)
+		addr = 6'b000000; rw = 1'b1; auto_inc = 1'b0; wr_data = 8'hA5;
+
+		expected_val = {auto_inc, rw, addr, wr_data};
+		dout = 1'b0;
+		
+		// Start Signal Stimulus
+		start = 1'b0;
+		repeat(5) @(posedge clk);
+		start = 1'b1;
+		repeat(1) @(posedge clk);
+		start = 1'b0;
+
+		wait(done);
+		repeat(5) @(posedge clk);
+
+		$finish;
+	end
+
+endmodule : tb_spi_master
